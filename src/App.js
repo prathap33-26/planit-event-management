06e1e6f1c79c50025ea1f26ae306cf0b2177205d
lalitha-2675 <239y1a05b5@ksrmce.ac.in>
2026-03-09 22:07:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import EventList from "./pages/client/EventList";
-import EventDetails from "./pages/client/EventDetails";
+import React from "react";
+import StaffDashboard from "./pages/staff/StaffDashboard";
 
 function App() {
   return (
-    <>
-    <h1>This is testing </h1>
-    <EventDetails/>
-    <BrowserRouter>
-      <Routes>
-
-        <Route path="/client/events" element={<EventList />} />
-
-        <Route path="/client/event/:id" element={<EventDetails />} />
-
-      </Routes>
-    </BrowserRouter>
-    </>
+    <div>
+      <StaffDashboard />
+    </div>
   );
 }
 
