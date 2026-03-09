7c6e4a9a83653bf6422c76309bf06b1c19832308
BMunendra <239y1a0518@ksrmce.ac.in>
2026-03-10 02:25:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import EventList from "./pages/client/EventList";
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import Navbar from "./components/layout/Navbar";
+
+import EventList from "./pages/planner/EventList";
+import CreateEvent from "./pages/planner/CreateEvent";
+import UpdateEvent from "./pages/planner/UpdateEvent";
+
+import ClientEventList from "./pages/client/EventList";
 import EventDetails from "./pages/client/EventDetails";
 
 function App() {
   return (
-    <>
-    <h1>This is testing </h1>
-    <EventDetails/>
-    <BrowserRouter>
-      <Routes>
+    <div className="app">
 
-        <Route path="/client/events" element={<EventList />} />
+      <div className="background-container" aria-hidden="true">
+        <div className="background-overlay"></div>
+      </div>
 
-        <Route path="/client/event/:id" element={<EventDetails />} />
+      <Navbar />
 
-      </Routes>
-    </BrowserRouter>
-    </>
+      <main className="main-content">
+
+        <Routes>
+
+          {/* Planner Routes */}
+          <Route path="/" element={<EventList />} />
+          <Route path="/events" element={<EventList />} />
+          <Route path="/create-event" element={<CreateEvent />} />
+          <Route path="/edit-event/:id" element={<UpdateEvent />} />
+
+          {/* Client Routes */}
+          <Route path="/client/events" element={<ClientEventList />} />
+          <Route path="/client/event/:id" element={<EventDetails />} />
+
+          {/* Default Redirect */}
+          <Route path="*" element={<Navigate to="/" replace />} />
+
+        </Routes>
+
+      </main>
+
+    </div>
   );
 }
 
